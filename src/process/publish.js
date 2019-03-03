13d538a2279a47674ee50527238d090522e2ae69
Josh Davenport <josh.davenport@tghp.co.uk>
2019-03-03 19:54:53 +0000

    Added logging output for publish to indicate when the public feed has changed

--- src/process/publish.js
+++ src/process/publish.js
@@ -5,8 +5,28 @@ import path from 'path';
 export default async () => {
 	await getFeeds().reduce(async (feedPromise, feed) => {
 		await feedPromise;
+
+		console.log(`[${feed.getId()}] Processing feed`);
+
+		const publicFeedPath = path.resolve(__dirname, `../../public/feeds/${feed.getKey()}.xml`);
+		let publicFeedSize = null;
+
+		if(!fs.existsSync(publicFeedPath)) {
+			console.log(`[${feed.getId()}] Creating new public feed`);
+		} else {
+			const publicFeedStat = fs.statSync(publicFeedPath);
+			publicFeedSize = publicFeedStat.size;
+		}
+
 		const xml = await feed.getPodcastXml()
 
-		fs.writeFileSync(path.resolve(__dirname, `../../public/feeds/${feed.getKey()}.xml`), xml);
+		fs.writeFileSync(publicFeedPath, xml);
+
+		if(publicFeedSize) {
+			const publicFeedStat = fs.statSync(publicFeedPath);
+			if(publicFeedSize !== publicFeedStat.size) {
+				console.log(`[${feed.getId()}] Public feed changed`);
+			}
+		}
 	}, Promise.resolve());
 }
